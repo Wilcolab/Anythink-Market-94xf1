6374a334f8578effd4280e7d4d79eef17c75b497
Rup-Royofficial <rupayanroy981@gmail.com>
2022-09-08 10:04:25 +0530

    formatted-code

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -55,6 +55,10 @@ router.get("/", auth.optional, function(req, res, next) {
     query.tagList = { $in: [req.query.tag] };
   }
 
+  if  (typeof req.query.title !== "undefined") {
+    query.title =  { "$regex": req.query.title, "$options": "i" };
+  }
+
   Promise.all([
     req.query.seller ? User.findOne({ username: req.query.seller }) : null,
     req.query.favorited ? User.findOne({ username: req.query.favorited }) : null
